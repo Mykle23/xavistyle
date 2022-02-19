854782cedf70719f11ba86ba92259789a70c87dd
Woxstone <nachomerling@gmail.com>
2022-02-19 18:52:37 +0100

    expenses test

---
+++ test/expenses.test.js
@@ -0,0 +1,17 @@
+import { Expenses } from '../src/expenses/expenses.js'
+
+describe('Test about the methods', () => {
+    it('Verify the return of add', () => {
+        const defaultExp = {
+            money: 0,
+            concept: '',
+            date: 2
+        };
+        const defaultUser = 25;
+        const expected = `${defaultExp.date} ${defaultExp.money} "${defaultExp.concept}"`
+
+        const result = Expenses.add(defaultUser, defaultExp);
+
+        expect(result).toEqual(expected);
+    });
+});
