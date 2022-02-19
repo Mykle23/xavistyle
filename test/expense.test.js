0f55e845898365e2dea37c6f6fc2324848eb6120
Woxstone <nachomerling@gmail.com>
2022-02-19 18:39:16 +0100

    expense test

---
+++ test/expense.test.js
@@ -0,0 +1,37 @@
+import { Expense } from '../src/expenses/expense.js';
+
+describe('test of the constructor of Expense', () => {
+     const exp = {
+            money: 0,
+            concept: '',
+            date: 2
+        };
+    it('should be 3 properties money, concept and date', () => {      
+        const defaultExp = new Expense(exp);
+
+        expect(defaultExp.money).toBeDefined();
+        expect(defaultExp.concept).toBeDefined();
+        expect(defaultExp.date).toBeDefined();
+    });
+
+    it('if date is undefined must be "today"',() => {
+        exp.date = undefined;
+        const defaultExp = new Expense(exp);
+
+        expect(defaultExp.date).toEqual('today');
+    });
+});
+
+describe('test about the methods', () => {
+    it('description methods should return the description of the expense', () => {
+        const exp = {
+            money: 0,
+            concept: '',
+            date: 2
+        };
+        const expected = `${exp.date} ${exp.money} "${exp.concept}"`;
+        const defaultExp = new Expense(exp);
+
+        expect(defaultExp.description()).toEqual(expected);
+    });
+});
