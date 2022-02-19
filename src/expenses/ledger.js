07a8e61f0cf6ff00d24ebac31022bd4b8ccbb3fd
Woxstone <nachomerling@gmail.com>
2022-02-19 20:23:37 +0100

    persintenci de ledger

--- src/expenses/ledger.js
+++ src/expenses/ledger.js
@@ -1,3 +1,5 @@
+import * as fs from 'fs';
+
 class Ledger{
     static collection = {}
 
@@ -5,6 +7,20 @@ class Ledger{
         if(!this.collection[user_id])this.collection[user_id]=[];
         let bag = this.collection[user_id]
         bag.push(expense)
+        if (!(Ledger.save())) {
+            throw 'Error in saving ledger';
+        };
+    }
+
+    static save() {
+        try {
+            fs.writeFileSync(process.env.DATA_FILE_EXPENESES, JSON.stringify(this.collection));
+            console.log('Sucess in saving');
+            return true;
+        } catch (err) {
+            console.error(err);
+            return false;
+        }
     }
 }
 
