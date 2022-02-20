f41126d76e451521a471a92b2010cfd06c562123
Fernando <motercode@gmail.com>
2022-02-20 16:01:28 +0100

    otromas

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -9,15 +9,12 @@ const fs = jest.createMockFromModule('fs');
 fs.writeFileSync.mockReturnValueOnce(true).mockImplementation(() => {
     throw new Error();
   });
-<<<<<<< HEAD
 
 fs.readFileSync.mockReturnValueOnce('firstCalledWeDontNEED').mockReturnValueOnce({ fer_id: [ {} ] }).mockImplementation(() => {
   throw new Error;
 });  
 
 
-=======
->>>>>>> e97d6e8411be6d5b747584e981055b04cf4b315d
 
 fs.readFileSync = jest.fn();
 fs.readFileSync.mockImplementation(()=> ({ fer_id: [ {} ] }));
