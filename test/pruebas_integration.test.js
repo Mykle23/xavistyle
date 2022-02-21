06e2970553109c45b5c048d44493be83172782e8
mrcaffeine98 <mrcaffeine98@gmail.com>
2022-02-21 12:38:42 +0100

    primer commit

--- test/pruebas_integration.test.js
+++ test/pruebas_integration.test.js
@@ -17,9 +17,9 @@ const fechaDeHoy = new Intl.DateTimeFormat('en-US', {
   let resultMessage = [];
  
   // testing actions
-  resultMessage.push(await runIntegrationTest('help command test', "/help","hola majete", 0));
-  resultMessage.push(await runIntegrationTest('gastos command test', '/gastos','gasto registrado: today 35 "copas"', 5));
-  resultMessage.push(await runIntegrationTest('start command test', "/start","Soy el botijo de los Gost", 5));
+  resultMessage.push(await runIntegrationTest('test de creacion de nuevo usuario command test', "/nuevo_usuario","", 0));
+  // resultMessage.push(await runIntegrationTest('gastos command test', '/gastos','gasto registrado: today 35 "copas"', 5));
+  // resultMessage.push(await runIntegrationTest('start command test', "/start","Soy el botijo de los Gost", 5));
 
   showTestResults(resultMessage);
   process.exit(1);
